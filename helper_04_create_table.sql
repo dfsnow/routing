a6b2e492590235f2950205420bf60b4e398e2ec1
Dan Snow <dfsnow@uchicago.edu>
2018-05-02 05:10:30 +0000

    Remove ignored files

--- helper_04_create_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS times;
-CREATE TABLE times (
-    id           serial PRIMARY KEY,	
-    origin       bigint,
-    destination  bigint,
-    agg_cost     float8
-);
-
-ANALYZE times;
